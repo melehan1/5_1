62183cdf6c10f7488c3e32a8522cb6d2b958c52a
Дмитрий <melehan@mail.ru>
2024-06-26 11:42:17 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -2,9 +2,7 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-
-        #self.go_to()
-
+        
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors or new_floor < 1:
             print('Такого этажа не существует')
